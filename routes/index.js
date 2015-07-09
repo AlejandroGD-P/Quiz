5ec82fc2a749ea6570f8e67e1ad845d272c09d45
AlejandroGD-P <alejandrogd-p@hotmail.com>
2015-07-09 22:01:10 +0200

    Implementando Rama Creditos
    Añadir pagina autor

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
+var quizController = require('../controllers/author_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -9,5 +10,7 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+router.get('/author', quizController.author);
+
 
 module.exports = router;
